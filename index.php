2f1593a4b0d93118284dad105b9499fcd1c2daec
Arturvideos <71207325+Arturvideos@users.noreply.github.com>
2020-09-13 10:50:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+﻿<html><head>
+<title>Защищенная страница: Пожалуйста, введите пароль</title>
+<script language="Javascript"> 
+//prompt
+var password = prompt("Enter in the password""")
+location = "correct.html"
+else {location = "error.html"}
+//--></script>
